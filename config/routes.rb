feaf3eb07960a0a3ce1121ab6836af0cac974376
Hussnain Younas <hussnainyouns143@gmail.com>
2020-06-12 21:21:11 +0500

    secondcom_12june

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #start
-  root 'index#first'
-  get '/ammaar', to: 'index#ammaar'
-  get '/about', to: 'index#about_us'
-  get '/developers', to: 'index#developer'
+  root 'pages#index'
+
 end
