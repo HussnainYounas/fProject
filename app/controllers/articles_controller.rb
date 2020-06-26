a653e02d38cd8f6748835f3ac38bf044312e56a8
Hussnain Younas <hussnainyouns143@gmail.com>
2020-06-26 19:59:21 +0500

    styling of buttons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,21 +16,18 @@ class ArticlesController < ApplicationController
 		end
 	end
 	def show
-		# @article = Article.find(params[:id])
 	end
 
 	def edit
-		# @article = Article.find(params[:id])
 	end
 
 	def update
-		# @article = Article.find(params[:id])
 		if @article.update(article_params)
 		flash[:success] = "Article was updated"
 		redirect_to article_path(@article)
 			
 		else
-			# flash[:danger] = @article.errors.full_messages.to_sentence
+			flash[:danger] = @article.errors.full_messages.to_sentence
 			render :edit
 		end
 	end
@@ -38,9 +35,8 @@ class ArticlesController < ApplicationController
 		@article = Article.all
 	end
 	def destroy
-		# @article = Article.find(params[:id])
 		@article.destroy
-		flash[:success] = "Article was Deleted"
+		flash[:danger] = "Article was Deleted"
 		redirect_to articles_path
 	end
 
